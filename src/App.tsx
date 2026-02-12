19c2d35dc047e0472367e8f0d0ed7d30c6d0a489
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2026-02-12 22:09:06 +0100

    Feat: Add packages and dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,25 +6,14 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <h1>My Count</h1>
+
+        <h2> Count Value: {count} </h2>
+
+        <button onClick={() => setCount(prevCount => prevCount + 1)}>
+          Add Count
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
