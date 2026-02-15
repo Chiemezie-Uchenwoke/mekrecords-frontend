ecbfd20587ac8a22b6bb288587ec00882b61eb50
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2026-02-15 20:21:21 +0100

    Feat: Add Card component

---
+++ src/components/common/Card.tsx
@@ -0,0 +1,16 @@
+interface CardProps {
+  children: React.ReactNode
+  className?: string
+  onClick?: () => void
+}
+
+export const Card = ({ children, className = '', onClick }: CardProps) => {
+  return (
+    <div 
+      className={`bg-white dark:bg-gray-800 rounded-lg shadow-md p-6 ${className}`}
+      onClick={onClick}
+    >
+      {children}
+    </div>
+  )
+};
